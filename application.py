c1c67bdf796fc2891dc4b8613bb6e48719c3ccfc
mkorszunsands <MateuszK@mk-sands.local>
2016-10-30 23:13:37 +0100

    Add some code documentation

--- application.py
+++ application.py
@@ -48,17 +48,23 @@ def args_error_handler(err):
 
 
 def str_to_datetime(created_at):
+    # We have here ISO_8601 date format:
+    # * https://de.wikipedia.org/wiki/ISO_8601
+    # * http://stackoverflow.com/questions/969285/how-do-i-translate-a-iso-8601-datetime-string-into-a-python-datetime-object
     return dateutil.parser.parse(created_at)
 
 
 def set_last_commit(repo):
     response = requests.get(COMMITS_SEARCH_QUERY.format(repo['owner']['login'], repo['name']))
 
+    # If for some reason request failed, lets continue with basic repo info we have
     if response.status_code != 200:
         app.logger.error('Failed to fetch commits for repo=%s: %s', repo['name'], response.text)
         return repo
 
     last_commit = response.json()[0]
+
+    # May happen that repo has not commits so far. In this case we display nothing.
     repo['last_commit_sha'] = last_commit['sha'] if last_commit else None
     repo['last_commit_msg'] = last_commit['commit']['message'] if last_commit else None
     repo['last_commit_author'] = last_commit['commit']['author']['name'] if last_commit else None
